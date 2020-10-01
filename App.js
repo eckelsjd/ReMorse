e4e9f2f40904eb0137b4ea7b17c5d0877160d482
Anand Desai <31744791+desaianand1@users.noreply.github.com>
2020-09-30 21:09:00 -0400

    initial HelloWorld app

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TextInput } from 'react-native';
 
-export default function App() {
+const HelloWorldApp = () => {
+  const [value, onChangeText] = React.useState('Useless Placeholder');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+      <Text>Hello, world!</Text>
+      <Text>This is so cool</Text>
+      <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => onChangeText(text)}
+      value={value}
+    />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default HelloWorldApp;
